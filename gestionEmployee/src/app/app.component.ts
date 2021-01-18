1377aca5ed357eae9f33a6a8e175da6e4276302f
AyedNejmeddine <AyedNejmeddine@github.com>
2021-01-18 23:03:20 +0100

    version1

---
+++ gestionEmployee/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Gestion Employee : Agular 11 Spring Boot CRUD Full Stack Application';
+}
---
+++ gestionEmployee/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,48 @@
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss']
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [];
+
+  constructor(private router: Router,
+              private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.employeeService.getEmployeeList().subscribe(
+      data =>{
+        this.employees = data;
+      }
+    );
+  }
+
+  private getAllEmployees(): void{
+    this.employeeService.getEmployeeList().subscribe(
+      data => this.employees=data,
+      error => console.log(error)
+    );
+  }
+
+  detailsEmployee(id: number | undefined){
+    this.router.navigate(['details-employee', id])
+
+  }
+
+  updateEmployee(id: number | undefined){
+    this.router.navigate(['update-employee', id])
+  }
+
+  deleteEmployee(id: number | undefined){
+    this.employeeService.deleteEmployee(id).subscribe(
+      data => this.getAllEmployees(),
+      error => console.log(error)
+    );
+  }
+}
